467bfa688e04bdfb9413ebef53c08d13bbc2f7c3
A <AAAAA@AAAA.com>
2025-03-27 17:19:27 +0900

    하이퍼 파라미터 조정완료

--- 코드_공유.py
+++ 코드_공유.py
@@ -33,7 +33,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random
 
 param_grid = {
     "criterion" : ['gini', 'entropy'],
-    "max_depth" : [2,5],
+    "max_depth" : [2,3,4,5],
     "min_samples_split": [2,5,10],
     "min_samples_leaf" : [1,2]
 }
