b3ad3894f3480a7e49dd3c51662503b2de488ba7
B <BBBB@BBB.com>
2025-03-27 17:12:10 +0900

    XGB 모델 작업 수행

--- 코드_공유.py
+++ 코드_공유.py
@@ -8,7 +8,12 @@ from sklearn.datasets import load_wine
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 
+from sklearn.preprocessing import LabelEncoder
+
 import matplotlib.pyplot as plt
+
+from xgboost import XGBClassifier
+from xgboost import plot_importance, plot_tree
 
 wine = load_wine()
 
@@ -16,8 +21,14 @@ wine = load_wine()
 # target은 'target' 컬럼만을 대상으로 합니다.
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
-''' 코드 작성 바랍니다 '''
+''' 해당 부분은 직접 작성 바랍니다 '''
+df = pd.DataFrame(data=wine.data, columns= wine.feature_names)
+df['target'] = wine.target
 
+X = df.drop('target', axis=1)
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=42)
 
 ####### A 작업자 작업 수행 #######
 
@@ -25,7 +36,37 @@ wine = load_wine()
 
 
 
+
+
 ####### B 작업자 작업 수행 #######
 
-''' 코드 작성 바랍니다 '''
+
+
+# 하이퍼파라미터 범위지정
+
+params = {
+    "max_depth" : [3, 5, 7, 9, 15],
+    "learning_rate" : [0.1, 0.01, 0.001],
+    "n_estimators": [50, 100, 200, 300]
+}
+
+label_encoder = LabelEncoder()
+y_train_encoded = label_encoder.fit_transform(y_train)
+y_test_encoded = label_encoder.transform(y_test)
+
+xgb_model = XGBClassifier(random_state=42)
+xgb_model.fit(X_train, y_train_encoded)
+
+
+# 하이퍼파라미터 최적화
+grid_search = GridSearchCV(estimator=xgb_model, param_grid=params, cv=5, scoring='accuracy', n_jobs=-1)
+grid_search.fit(X_train, y_train_encoded)
+
+# 최적의 모델 선택
+best_model = grid_search.best_estimator_
+
+print("Best parameters:", grid_search.best_params_)
+print("Best accuracy:", grid_search.best_score_)
+
+
 
